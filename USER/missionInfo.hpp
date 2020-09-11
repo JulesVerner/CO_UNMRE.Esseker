bc0fe0ef6098a3bf327532de62a74e3430da7f09
Salbei <marc.heinze@muenster.de>
2020-09-11 19:06:41 +0200

    finished un loadout

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -2,9 +2,9 @@
 *   Legt allgemeine Information über die Mission fest.
 */
 
-author = "nomisum, Salbei & Jules für Gruppe Adler";                                               // Missionsersteller
-onLoadName = "CO UNMRE";                                                   // Name der Mission
+author = "Jules, nomisum & Salbei für Gruppe Adler";                            // Missionsersteller
+onLoadName = "CO UNMRE";                                                   		// Name der Mission
 onLoadMission = "";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
 loadScreen = "data\loadpic.paa";                                                // Ladebild
 overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
-overviewText = "";                                                              // Text, der in der Missionsauswahl angezeigt wird
+overviewText = "United Nations Mission in the Republic of Esseker";        // Text, der in der Missionsauswahl angezeigt wird
