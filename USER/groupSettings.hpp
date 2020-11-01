da99786a547a49c4c3b1903e88b9e3963a4e18f9
JulesVerner <julius.lenz@outlook.com>
2020-11-01 16:58:15 +0100

    loadout adjustments

--- USER/groupSettings.hpp
+++ USER/groupSettings.hpp
@@ -32,88 +32,88 @@
 *
 */
 
-class BLUFOR {
-    class Gamma {
-        groupName = "Gamma";
+// class BLUFOR {
+//     class Gamma {
+//         groupName = "Gamma";
 
-            class longrange {
-                defaultChannel = 1;
-                channel1 = "Command Channel";
-            };        
-    };
+//             class longrange {
+//                 defaultChannel = 1;
+//                 channel1 = "Command Channel";
+//             };        
+//     };
 
-    class Alpha {
-        groupName = "Alpha";
+//     class Alpha {
+//         groupName = "Alpha";
 
-        class shortrange {
-            defaultChannel = 1;
-            altChannel = 2;
-            channel1 = "Alpha 1";
-            channel2 = "Alpha 2";
-        };
+//         class shortrange {
+//             defaultChannel = 1;
+//             altChannel = 2;
+//             channel1 = "Alpha 1";
+//             channel2 = "Alpha 2";
+//         };
         
-        class longrange {
-            defaultChannel = 1;
-            channel1 = "Command Channel";            
-        };
-    };
+//         class longrange {
+//             defaultChannel = 1;
+//             channel1 = "Command Channel";            
+//         };
+//     };
 
-    class Alpha1 {
-        groupName = "Alpha 1";
+//     class Alpha1 {
+//         groupName = "Alpha 1";
 
-        class shortrange {
-            defaultChannel = 1;
-            channel1 = "Alpha 1";
-        };
-    };
+//         class shortrange {
+//             defaultChannel = 1;
+//             channel1 = "Alpha 1";
+//         };
+//     };
 
-    class Alpha2 {
-        groupName = "Alpha 2";
+//     class Alpha2 {
+//         groupName = "Alpha 2";
 
-        class shortrange {
-            defaultChannel = 1;
-            channel1 = "Alpha 2";
-        };
-    };       
+//         class shortrange {
+//             defaultChannel = 1;
+//             channel1 = "Alpha 2";
+//         };
+//     };       
 
-    class Bravo {
-        groupName = "Bravo";
+//     class Bravo {
+//         groupName = "Bravo";
 
-        class shortrange {
-            defaultChannel = 1;
-            altChannel = 2;
-            channel1 = "Bravo";
-        };
+//         class shortrange {
+//             defaultChannel = 1;
+//             altChannel = 2;
+//             channel1 = "Bravo";
+//         };
         
-        class longrange {
-            defaultChannel = 1;
-            channel1 = "Command Channel";            
-        };
-    };
+//         class longrange {
+//             defaultChannel = 1;
+//             channel1 = "Command Channel";            
+//         };
+//     };
 
-    class Bravo1 {
-        groupName = "Bravo 1";
+//     class Bravo1 {
+//         groupName = "Bravo 1";
 
-        class shortrange {
-            defaultChannel = 1;
-            channel1 = "Bravo";
-        };
-    };
+//         class shortrange {
+//             defaultChannel = 1;
+//             channel1 = "Bravo";
+//         };
+//     };
 
-    class Bravo2 {
-        groupName = "Bravo 2";
+//     class Bravo2 {
+//         groupName = "Bravo 2";
 
-        class shortrange {
-            defaultChannel = 1;
-            channel1 = "Bravo";
-        };
-    };      
-};
+//         class shortrange {
+//             defaultChannel = 1;
+//             channel1 = "Bravo";
+//         };
+//     };      
+// };
 
-class OPFOR {
+// class OPFOR {
 
-};
+// };
 
-class INDEPENDENT {
+// class INDEPENDENT {
 
-};
+// };
